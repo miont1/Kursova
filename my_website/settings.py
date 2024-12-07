27a6a719a5cca6dc5c95a9d81a3e7b042863d957
miont1 <a.sarain3202@gmail.com>
2024-12-07 22:59:44 +0200

    finish

--- my_website/settings.py
+++ my_website/settings.py
@@ -204,6 +204,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, "static/images")
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
